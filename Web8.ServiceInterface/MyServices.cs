31b16395ff8c99639ed4b29f5ccfc39a02b21ede
Darren Reid <darren@reidmail.org>
2023-09-26 16:05:34 +1000

    Initial commit

---
+++ Web8.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using Web8.ServiceModel;
+
+namespace Web8.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
---
+++ Web8/Configure.AppHost.cs
@@ -0,0 +1,25 @@
+using Funq;
+using ServiceStack;
+using Web8.ServiceInterface;
+
+[assembly: HostingStartup(typeof(Web8.AppHost))]
+
+namespace Web8;
+
+public class AppHost : AppHostBase, IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            // Configure ASP.NET Core IOC Dependencies
+        });
+
+    public AppHost() : base("Web8", typeof(MyServices).Assembly) {}
+
+    public override void Configure(Container container)
+    {
+        // Configure ServiceStack only IOC, Config & Plugins
+        SetConfig(new HostConfig {
+            UseSameSiteCookies = true,
+        });
+    }
+}
